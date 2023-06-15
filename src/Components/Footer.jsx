1cab72cf7b8d53ca9fd67a517f8b6bf457b557c6
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-06-15 15:39:03 +0200

    footer/links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,11 +7,12 @@ function Footer() {
     <div>
       <footer className="footer-container">
         <p>Este es el Footer de la pagina!</p>
+        
         <div className="footer-links">
-          <a href="sinNada">About US</a>
-          <a href="sinNada">Contact</a>
-          <a href="sinNada">Twitter</a>
-          <a href="sinNada">Facebook</a>
+          <a href="sinNada">About US </a>
+          <a href="sinNada">Contact </a>
+          <a href="sinNada">Twitter </a>
+          <a href="sinNada">Facebook </a>
         </div>
       </footer>
     </div>
