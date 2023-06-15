d845a3afb5728293c938d62e114b18401c22e3f6
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-06-15 15:53:26 +0200

    creacion componente boton join now

---
+++ src/Components/ButtonJoin.jsx
@@ -0,0 +1,12 @@
+import './ButtonJoin.css';
+import React from 'react';
+
+const ButtonJoin = () => {
+  return (
+    <button className="join-now">
+      Join Now
+    </button>
+  );
+};
+
+export default ButtonJoin;
