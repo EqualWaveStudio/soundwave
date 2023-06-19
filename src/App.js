6f4337d5b3d5f318965ed4092b06bc02c816f508
Laura <garch.laura@gmail.com>
2023-06-19 09:14:23 +0200

    solved app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import MainHome from './Home/MainHome';
+import MainJoin from './Join/MainJoin'
 import MainDiscover from './Discover/MainDiscover';
 import Footer from './Components/Footer';
 
