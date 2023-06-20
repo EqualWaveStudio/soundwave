20046acd49b2c8bbca7853ef90e34c5248e7a845
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-06-20 10:19:15 +0200

    Inclusión de hoover en MainDiscover.css para micro, albums y more

--- src/Discover/MainDiscover.test.js
+++ src/Discover/MainDiscover.test.js
@@ -2,43 +2,75 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import MainDiscover from './MainDiscover';
-//import { BabelFile } from '@babel/core';
-//import { describe } from 'node:test';
 
-describe('Discover', () => {
-    test('render h1 component', () => {
-        render(
-            <MemoryRouter>
-              <MainDiscover />
-            </MemoryRouter>
-          );
-        const h1Element = screen.getByText('Discover new music');
-        expect(h1Element).toBeInTheDocument();
-    })
+describe('Render h1 component', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <MainDiscover />
+      </MemoryRouter>
+    );
+  })
+  
+  test('Should have an h1', () => {
+    const h1Element = screen.getByRole('heading', { level: 1 });
+
+    expect(h1Element).toBeInTheDocument();
+  })
+
+  test('h1 should have a text', () => {
+    const h1Element = screen.getByText('Discover new music');
+
+    expect(h1Element).toBeInTheDocument();
+  })
+
+  test('If the text does not match', () => {
+    const h1Element = screen.getByText('over new music', { exact: false });
+
+    expect(h1Element).toBeInTheDocument();
+  })
 })
 
-describe('Discover', () => {
-    test('render p component', () => {
-        render(
-            <MemoryRouter>
-              <MainDiscover />
-            </MemoryRouter>
-          );
-        const pElement = screen.getByText('By joing you can benefit by listening to the latest albums releases.');
-        expect(pElement).toBeInTheDocument();
-    })
+describe('Render p component', () => {
+  test('P should have a text', () => {
+    render(
+      <MemoryRouter>
+        <MainDiscover />
+      </MemoryRouter>
+    );
+  
+    const pElement = screen.getByText('By joing you can benefit by listening to the latest albums releases.');
+
+    expect(pElement).toBeInTheDocument();
+  })
 })
 
-/*describe('Discover', () => {
-    test('Muestra el h1 correctamente', () => {
-        render(
-            <MemoryRouter>
-              <Discover />
-            </MemoryRouter>
-          );
-        const h1 = 'Discover new music';
-        const { getByText } = render(Discover h1={h1}, screen);
-        const h1Element = getByText(h1);
-        expect(h1Element).toBeInTheDocument();
-    })
-})*/
+describe('Alt text for the images', () => {  
+  it('If the microphone image fails, the alternate text of the microphone icon image should appear', () => {   
+    const { getByAltText } = render(<MainDiscover />);    
+    const imagen = getByAltText(/microphone icon/i); 
+
+    expect(imagen).toBeInTheDocument();  
+  });
+
+  it('If the albums image fails, the alternate text of the albums icon image should appear', () => {   
+    const { getByAltText } = render(<MainDiscover />);    
+    const imagen = getByAltText(/albums icon/i);    
+
+    expect(imagen).toBeInTheDocument();  
+  });
+
+  it('If the more image fails, the alternate text of the more icon image should appear', () => {   
+    const { getByAltText } = render(<MainDiscover />);    
+    const imagen = getByAltText(/more icon/i);  
+
+    expect(imagen).toBeInTheDocument();  
+  });
+
+  it('If the covers image fails, the alternate text of the collage of album covers image should appear', () => {   
+    const { getByAltText } = render(<MainDiscover />);    
+    const imagen = getByAltText(/collage of album covers/i);  
+
+    expect(imagen).toBeInTheDocument();  
+  });
+});
