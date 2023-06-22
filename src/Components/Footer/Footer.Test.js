f32f41484ce440f98cfd21c3e6221ddf9c5e1e29
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-06-22 11:19:20 +0200

    Footer/Test

---
+++ src/Components/Footer/Footer.Test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Footer from './Footer.jsx';
+
+describe('Footer', () => {
+    test('renders Logo component', () => {
+      render(
+        <MemoryRouter>
+          <Footer />
+        </MemoryRouter>
+      );
+      const logoElement = screen.getByAltText('LogoTwitter');
+      const logoTitleElement = screen.getByText('Twitter');
+      expect(logoElement).toBeInTheDocument();
+      expect(logoTitleElement).toBeInTheDocument();
+    });
+
+  test('renders text', () => {
+    render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    );
+    const textElement = screen.getByText('About US');
+    expect(textElement).toBeInTheDocument();
+  });
+});
