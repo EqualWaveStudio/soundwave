0a09ab62b7e87dfa888c608ae9749b7bbb983ab8
Laura <garch.laura@gmail.com>
2023-06-23 10:48:56 +0200

    test

--- src/Components/FormValidation/FormValidation.test.js
+++ src/Components/FormValidation/FormValidation.test.js
@@ -2,35 +2,26 @@ import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import FormValidation from './FormValidation';
+import { MemoryRouter } from 'react-router-dom';
 
 let container = null;
 
 beforeEach(() => {
-  // Contenedor <div> en el DOM
   container = document.createElement('div');
-  // Contenedor al cuerpo del documento
   document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // se limpia
   unmountComponentAtNode(container);
-  // Se elimina el container
   container.remove();
-  // se reestablece
   container = null;
 });
 
 it('should render the form', () => {
-  /*función act, es una función proporcionada por react-dom/test-utils que se utiliza para envolver interacciones 
-  y aserciones con componentes de React. Su propósito principal es asegurarse de 
-  que todas las actualizaciones del componente se completen antes de continuar con las siguientes acciones*/
   act(() => {
-// Renderizamos el componente FormValidation 
-    render(<FormValidation />, container);
+    render(<MemoryRouter><FormValidation /></MemoryRouter>, container);
   });
 
-  // Realizamos las aserciones para verificar que los elementos esperados estén presentes en el contenedor renderizado
   expect(container.querySelector('label[for="name"]')).toBeInTheDocument();
   expect(container.querySelector('label[for="email"]')).toBeInTheDocument();
   expect(container.querySelector('label[for="password"]')).toBeInTheDocument();
